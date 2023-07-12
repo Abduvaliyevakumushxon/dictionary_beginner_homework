5368cb8b1eff961367e6fcba66f3507a48e56eb0
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2023-07-12 17:22:31 +0500

    14-vazifa tugadi

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -6,4 +6,15 @@ def sum_dict_values(data: dict) -> int:
     Returns:
         int: The sum of all values in the dictionary
     '''
-    return 
+    s=0
+    for i in range(len(data)):
+        last=data.popitem()
+        s+=last[1]
+    return s
+data = {
+    'a': 1, 
+    'b': 2, 
+    'c': 3
+  }
+a=sum_dict_values(data)
+print(a)
