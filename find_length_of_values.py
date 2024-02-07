e59aa68181cd43252402c87b83e255b740e28961
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-02-07 17:32:28 +0500

    4-vazifa bajarildi

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,20 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    data = {
+    'a': 'abc',
+    'b': 'def', 
+    'c': 'ghi'
+  }
+    s="a"
+    d="b"
+    h="c"
+    q=data.get(s)
+    r=data.get(d)
+    g=data.get(h)
+    return len(q+r+g)
+print(find_length_of_values(data = {
+    'a': 'abc',
+    'b': 'def', 
+    'c': 'ghi'
+  }))
